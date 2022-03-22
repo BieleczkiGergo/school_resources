35c54f267a01a07421f88c1c4f839934fb086580
Geri <bgergo8b@gmail.com>
2022-03-22 12:03:06 +0100

    Worked some on JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,27 @@
 
 
 
-function function1 () {
+function function1 (){
     document.getElementById('basic').innerHTML = "Content changed";
 
 }
 
-function function2 () {
+function function2 (){
     document.getElementById('basic').style.fontFamily = "verdana";
 
 }
-    
+
+function blockTest(){
+    let num1 = 2;
+    var num2 = 4;
+    var inblock = document.getElementById("inblock");
+    var outblock = document.getElementById("outblock");
+
+    {
+        let num1 = 3;
+        var num2 = 5;
+        inblock.innerHTML = "" + num1 + ", " + num2;
+    }
+
+    outblock.innerHTML = "" + num1 + ", " + num2;
+}
