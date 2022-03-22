43bb570650b8166234038912993cb5131abb29de
Geri <bgergo8b@gmail.com>
2022-03-22 11:37:03 +0100

    Added JavaScript folder + progress

---
+++ JavaScript/inscript.js
@@ -0,0 +1,3 @@
+document.getElementById("inscript").innerHTML = "POW: the code works";
+document.write("<p>POW: you can add html code</p>");
+
---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+
+
+
+function function1 () {
+    document.getElementById('basic').innerHTML = "Content changed";
+
+}
+
+function function2 () {
+    document.getElementById('basic').style.fontFamily = "verdana";
+
+}
+    
